42f9fc7131a5ca405d891c0427902b89356acc8a
Nik <144826650+Nikiti7@users.noreply.github.com>
2024-03-16 21:42:24 +0300

    Add files via upload

---
+++ hh_geocoding_template/geocoders/memorized_tree_geocoder.py
@@ -0,0 +1,24 @@
+from api import TreeNode, API
+from geocoders.geocoder import Geocoder
+
+
+# Инверсия дерева
+class MemorizedTreeGeocoder(Geocoder):
+    def __init__(self, samples: int | None = None, data: list[TreeNode] | None = None):
+        super().__init__(samples=samples)
+        if data is None:
+            self.__data = API.get_areas()
+        else:
+            self.__data = data
+        self.memory = {}
+
+        for country in self.__data:
+            for area in country.areas:
+                for sity in area.areas:
+                    self.memory[sity.id] = f"{country.name} {area.name} {sity.name}"
+                self.memory[area.id] = f"{country.name} {area.name}"
+            self.memory[country.id] = f"{country.name}"
+
+    def _apply_geocoding(self, area_id: str) -> str:
+        return self.memory.get(area_id)
+  
